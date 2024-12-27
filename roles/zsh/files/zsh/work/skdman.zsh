105370f346ab8aabd3573bfc4ff460a3612380dc
Maciej Brzęczkowski <maciej.brzeczkowski@protonmail.com>
2024-12-27 21:11:58 +0100

    Remove sdkman file
    Not used anymore

--- roles/zsh/files/zsh/work/skdman.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-alias sdk-tree='tree -L 2 ~/.sdkman/candidates'
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
